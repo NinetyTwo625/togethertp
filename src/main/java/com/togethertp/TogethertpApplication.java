43fbe8b547f889564b6c93f592cf8d282c3710b2
NinetyTwo625 <dytk_@naver.com>
2023-03-17 10:00:08 +0900

    Initial commit

---
+++ src/main/java/com/togethertp/TogethertpApplication.java
@@ -0,0 +1,13 @@
+package com.togethertp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogethertpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogethertpApplication.class, args);
+	}
+
+}
